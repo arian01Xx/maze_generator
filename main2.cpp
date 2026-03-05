8ffa44d339cf1d7e4abe19cbf93bd829e3e7c772
arian01Xx <arian_joseph@outlook.es>
2026-03-04 23:31:47 -0500

    continuing

--- main2.cpp
+++ main2.cpp
@@ -45,53 +45,82 @@ struct World{
                     if(world[new_i][new_j]==1) neighbor++;
                 }
 
-                if(neighbor>=4) continue;
+                building(neighbor, i, j, gen, dis);  
+            }
+        }
+    }
 
-                //SOLO SUMA CUADROS, NO HAY DESCARTE AUN
-                if(world[i][j]==0 && neighbor<2){
-                    int c=neighbor;
-                    int x, y;
-                    while(c!=2){
-                        int k=dis(gen);
-                        int new_i=center_x[k]+i;
-                        int new_j=center_y[k]+j;
-                        world[new_i][new_j]=1;
-                        c++;
-                    }
-                }
+    void building(int& neighbor, int& i, int& j, std::mt19937& gen, std::uniform_int_distribution<>& dis){
+        if(neighbor>=4) return;
 
-                if(world[i][j]==0 && neighbor>=3){
-                    int c=0;
-                    int x, y;
-                    while(c!=2){
-                        int k=dis(gen);
-                        int new_i=center_x[k]+i;
-                        int new_j=center_y[k]+j;
+        //SOLO SUMA CUADROS, NO HAY DESCARTE AUN
+        if(world[i][j]==0 && neighbor<2){
+            int c=neighbor;
+            int x, y;
+            while(c!=2){
+                int k=dis(gen);
+                int new_i=center_x[k]+i;
+                int new_j=center_y[k]+j;
+                world[new_i][new_j]=1;
+                c++;
+            }
+        }
 
-                        if(new_i==0 || new_i==49) continue;
-                        if(new_j==0 || new_j==49) continue;
+        if(world[i][j]==0 && neighbor>=3){
+            int c=0;
+            int x, y;
+            while(c!=2){
+                int k=dis(gen);
+                int new_i=center_x[k]+i;
+                int new_j=center_y[k]+j;
 
-                        world[new_i][new_j]=0;
-                        c++;
-                    }
-                }
+                if(new_i==0 || new_i==49) continue;
+                if(new_j==0 || new_j==49) continue;
+
+                world[new_i][new_j]=0;
+                c++;
             }
         }
     }
 
     void draw(sf::RenderWindow& window){
         sf::RectangleShape wall(sf::Vector2f(TILE, TILE));
-        wall.setFillColor(sf::Color::Cyan);
+        sf::RectangleShape Start(sf::Vector2f(TILE-1, TILE-1));
+        sf::RectangleShape End(sf::Vector2f(TILE-1, TILE-1));
 
         for(int i=0; i<50; i++){
             for(int j=0; j<50; j++){
                 if(world[i][j]==1){
+                    wall.setFillColor(sf::Color::Cyan);
                     wall.setPosition({float(j*TILE), float(i*TILE)});
                     window.draw(wall);
+                }else if(world[i][j]==2){
+                    Start.setFillColor(sf::Color::Green);
+                    Start.setPosition({float(j*TILE), float(i*TILE)});
+                    window.draw(Start);
+                }else if(world[i][j]==3){
+                    End.setFillColor(sf::Color::Red);
+                    End.setPosition({float(j*TILE), float(i*TILE)});
+                    window.draw(End);
                 }
             }
         }
     }
+};
+
+struct A_path{
+    //f(n)=g(n)+h(n)
+    //start -g(n)-> current -h(n)-> goal
+    //coste real g(n(k))=sumatoria de i=0 hasta k-1 en la forma w(n(i), n(i+1))
+    //w(n(i), n(i+1)) representa el peso de la arista que conecta el nodo n(i) al nodo n(i+1)
+    //funcion heuristica h(n)=
+    //distancia manhattan h(n)=|x1-x2|+|y1-y2|
+    //distancia euclidiana h(n)=raiz cuadrada((x1-x2)^2+(y1-y2)^2)
+    /*
+     coste total estimado f(n)
+     g(n) coste real desde el inicio hasta el nodo actual
+     h(n) coste estimado desde el nodo actual hasta el objetivo
+     * */
 };
 
 void execute(){
@@ -107,11 +136,33 @@ void execute(){
 
     window.setFramerateLimit(60);
 
+    enum Target{start, end};
+    Target tar;
+    tar=start;
+
     while(window.isOpen()){
         while(const std::optional event=window.pollEvent()){
-            if(event->is<sf::Event::Closed>()) window.close();
-            
-        } 
+            if(event->is<sf::Event::Closed>()) window.close(); 
+
+            if(const auto* mousePressed=event->getIf<sf::Event::MouseButtonPressed>()){
+                if(mousePressed->button==sf::Mouse::Button::Left){
+                    int col=mousePressed->position.x/TILE;
+                    int row=mousePressed->position.y/TILE;
+
+                    if(row>=0 && row<=49 && col>=0 && col<=49){
+                        if(_w.world[row][col]==0){
+                            if(tar==start){
+                                _w.world[row][col]=2;
+                                tar=end;
+                            }else{
+                                _w.world[row][col]=3;
+                                tar=start;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         
         window.clear();
         _w.draw(window);
